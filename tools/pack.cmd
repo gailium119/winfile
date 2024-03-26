da09ab051f9b31665f0d61e1d93d92f7e19e8017
Administrator <Administrator>
2024-03-26 10:57:11 +0800

    remove obj

---
+++ tools/pack.cmd
@@ -0,0 +1,13 @@
+cd /d %~dp0
+echo off && cls
+md temp
+for %%h in (Debug Release) do (
+for %%i in (ARM ARM64 x64 Win32) do (
+copy /y ..\src\%%i\%%h\WinFile.exe temp\Winfile-%%i-%%h.exe
+copy /y ..\src\%%i\%%h\WinFile.pdb temp\Winfile-%%i-%%h.pdb
+for %%j in (ExeView FileSignatureInfo VerifyResources) do (
+copy /y .\%%j\%%i\%%h\%%j.exe temp\%%j-%%i-%%h.exe
+copy /y .\%%j\%%i\%%h\%%j.exe temp\%%j-%%i-%%h.pdb
+)
+)
+)
